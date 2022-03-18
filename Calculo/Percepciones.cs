34e501312a36a4f414093958027127d123757c97
ZaredMaldon <zaredosvaldom@gmail.com>
2022-03-18 00:57:57 -0600

    Separado de pantallas
    Se agrego el esqueleto de pantallas que se necesitaran

---
+++ Calculo/Percepciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_MAD.Percepciones_y_deducciones
+{
+    public partial class Percepciones : Form
+    {
+        public Percepciones()
+        {
+            InitializeComponent();
+        }
+    }
+}

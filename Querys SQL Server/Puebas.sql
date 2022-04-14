8d5523f08db39f2653dc5de15f3521680662359c
ZaredMaldon <zaredosvaldom@gmail.com>
2022-04-13 22:46:50 -0500

    Query

---
+++ Querys SQL Server/Puebas.sql
@@ -0,0 +1,6 @@
+insert into Usuarios values ('Zaza','asdf4651*A');
+Insert into Gerentes (Nombre,APaterno,AMaterno,idUsuarios) values ('Zared','Maldonado','Rodriguez',1);
+
+select * from Usuarios;
+select * from Gerentes;
+Update Usuarios set Contraseña='Zaza*1234';

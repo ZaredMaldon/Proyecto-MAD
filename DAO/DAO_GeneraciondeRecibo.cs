5385340fa69fce08138a17825a2b9696b21341f0
samycharless <samycharles37@gmail.com>
2022-05-13 17:17:28 -0500

    DAO generacion de recibo

---
+++ DAO/DAO_GeneraciondeRecibo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_MAD.DAO
+{
+    internal class DAO_GeneraciondeRecibo
+    {
+       public static float SueldoBruto { get; set; }
+        public static float SueldoNeto { get; set; }
+        public static string Banco { get; set; }
+        public static int NoCuenta { get; set; }
+        public static DateTime FechaNomina { get; set; }
+
+    }
+}
